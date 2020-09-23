cbdb7ffcb4548c32065a6028fc94d7b62bc0d827
AngelaFang <1823476439@qq.com>
2020-09-23 17:00:15 +0800

    修改路径deployed

---
+++ precache-manifest.810838b9e419c43647bed123038ba8d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422d12e07f3a49e48972dfea81c12f45",
+    "url": "/view-money-shasha/index.html"
+  },
+  {
+    "revision": "d1f0bf39116e96a549e4",
+    "url": "/view-money-shasha/static/css/main.1fb8231d.chunk.css"
+  },
+  {
+    "revision": "dc1134af58ea003fad70",
+    "url": "/view-money-shasha/static/js/2.2298e01c.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/view-money-shasha/static/js/2.2298e01c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f0bf39116e96a549e4",
+    "url": "/view-money-shasha/static/js/main.aee7592e.chunk.js"
+  },
+  {
+    "revision": "c27edb96d5b83d6b3fc2",
+    "url": "/view-money-shasha/static/js/runtime-main.c3e1b6e2.js"
+  }
+]);
